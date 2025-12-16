ea37d1e0c81786d21873fd4698f1a616132a01b7
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2025-12-16 23:27:20 +0000

    Create wallet_screen.dart

---
+++ lib/wallet_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WalletScreen extends StatelessWidget {
+  const WalletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Wallet")),
+      body: const Center(
+        child: Text(
+          "Wallet balance will appear here",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
